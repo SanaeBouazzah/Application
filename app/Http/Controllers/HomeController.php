41464c6216e5cb296a76e799d7c3da9bc7434e11
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-13 01:57:11 +0200

    ll

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+      $allCategories = ['Category 1', 'Category 2'];
+      return view(view:'index', ['categories' => $allCategories]);
+    }
+}
