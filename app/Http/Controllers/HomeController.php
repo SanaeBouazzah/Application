a731b9c569023e51bd9ebd3fbe51a8f5ce892bf6
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-13 18:33:20 +0200

    getting data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index(){
       $allCategories =  Category::all();
+      $posts = Post::latest()->get();
       return view('index', ['categories' => $allCategories]);
     }
 }
