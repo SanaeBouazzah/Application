d57f4624fd71774fdf871d0721797d2039fac010
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-13 19:11:20 +0200

    bring-data-categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,12 @@ use App\Models\Post;
 class HomeController extends Controller
 {
     public function index(){
-      $allCategories =  Category::all();
-      $posts = Post::latest()->get();
-      return view('index', ['categories' => $allCategories]);
+      $categories =  Category::all();
+      $posts = Post::when(request(key:'category_id'), function($query){
+        $query->where('category_id', request(key:'category_id'));
+      })
+      ->latest()
+      ->get();
+      return view('index', compact('categories', 'posts'));
     }
 }
